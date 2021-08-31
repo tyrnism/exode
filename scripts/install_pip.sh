da130ae80c157cca4a632b9dca44a64f0c99c578
tyrnis <tyrnis@elatha.eu>
2021-08-31 11:54:28 +0900

    Add logs and scripts directories

---
+++ scripts/install_pip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# bunch of package needed (or maybe not) to run the script
+
+/usr/bin/python3 -m pip install --upgrade pip
+
+pip install -U cryptography
+pip install -U beem
+pip install -U discord.py
+pip install -U python-dotenv
+pip install mysql-connector-python
+
+
+
+
