5e475aa59b78984d926b69ce1f9638e2051e2c8f
Tyrnis <tyrnis@elatha.eu>
2024-10-04 09:47:23 +0900

    Kill previous screen

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+source env.sh
+
+screen -S EXODE_READ -X quit
+screen -S EXODE_ANALYSIS -X quit
+
 screen -dmS EXODE_READ ./start_read.sh
 screen -dmS EXODE_ANALYSIS ./start_analysis.sh
